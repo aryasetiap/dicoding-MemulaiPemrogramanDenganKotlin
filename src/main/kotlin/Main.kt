14c2f33a6fb6c1d0222ada873e6568bd05cb0b90
aryasetiap <aryasetiap.code@gmail.com>
2024-12-09 19:14:39 +0700

    belajar Raw String

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,16 +3,14 @@ package com.dicoding.kotlin
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val statement = "Kotlin is \"Awesome!\""
-    val name = "Unicode test: \u00A9"
+    val line = """
+        Line 1
+        Line 2
+        Line 3
+        Line 4
+    """.trimIndent()
 
-//    Escaped String memungkinkan kita untuk mengurangi ambiguitas nilai yang berada di dalam sebuah String.
-//    \t: menambah tab ke dalam teks.
-//    \n: membuat baris baru di dalam teks.
-//    \’: menambah karakter single quote kedalam teks.
-//    \”: menambah karakter double quote kedalam teks.
-//    \\: menambah karakter backslash kedalam teks.
+    print(line)
 
-    println(statement)
-    print(name)
+//    Raw String memungkinkan untuk menuliskan multiline dan arbitrary text.
 }
