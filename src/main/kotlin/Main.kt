267dfbcc2022e47f7d69e975047a4d6bf538cb37
aryasetiap <aryasetiap.code@gmail.com>
2024-12-09 19:37:09 +0700

    belajar Boolean

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,22 +3,38 @@ package com.dicoding.kotlin
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val openHours = 7
-    val now = 7
-    val office: String = if (now > 7) {
-        "Office already open"
-    } else if (now == openHours){
-        "Wait a minute, office will be open"
+    /*
+    Boolean adalah tipe data yang hanya memiliki dua nilai, yaitu true dan false.
+    Ada tiga operator utama yang digunakan dengan Boolean:
+    */
+
+    val officeOpen = 7
+    val officeClosed = 16
+    val now = 20
+
+    /*
+    1. Conjunction (AND)
+    Operator && mengembalikan nilai true jika semua kondisi yang dievaluasi bernilai true.
+    */
+    val isOpen = now >= officeOpen && now <= officeClosed
+
+    println("Office is open : $isOpen")
+
+    /*
+    2. Disjunction (OR)
+    Operator || mengembalikan nilai true jika salah satu kondisi yang dievaluasi bernilai true.
+    */
+    val isClose = now < officeOpen || now > officeClosed
+
+    println("Office is closed : $isClose")
+
+    /*
+    3. Negation (NOT)
+    Operator ! digunakan untuk melakukan negasi pada hasil evaluasi kondisi, mengubah true menjadi false dan sebaliknya.
+    */
+    if (!isOpen) {
+        println("Office is closed")
     } else {
-        "Office is closed"
+        println("Office is open")
     }
-
-    print(office)
-
-//    If expressions digunakan untuk menginisialisasi nilai variabel berdasarkan kondisi tertentu.
-//    If akan mengeksekusi statement atau expression jika kondisi bernilai true,
-//    dan melewatkan proses jika bernilai false.
-//    Jika if digunakan untuk mengembalikan atau menetapkan nilai variabel,
-//    maka harus memiliki branch else.
-//    Kotlin tidak mendukung ternary operator karena perannya sudah digantikan oleh if expressions
 }
