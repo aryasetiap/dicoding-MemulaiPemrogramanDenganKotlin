3ab537d66af8a56e36d15b3173b2134cbe0b1206
aryasetiap <aryasetiap.code@gmail.com>
2024-12-09 19:54:57 +0700

    belajar tipe data Numbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,79 @@
 package com.dicoding.kotlin
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     /*
-    Boolean adalah tipe data yang hanya memiliki dua nilai, yaitu true dan false.
-    Ada tiga operator utama yang digunakan dengan Boolean:
+    Numbers dalam pemrograman Kotlin.
+    Tipe data Number digunakan untuk menyimpan nilai dalam bentuk numerik.
+    Kotlin memiliki beberapa tipe data Number, termasuk Int, Long, Short, Byte, Double, dan Float.
+    Setiap tipe data memiliki ukuran dan rentang nilai yang berbeda-beda.
     */
-
-    val officeOpen = 7
-    val officeClosed = 16
-    val now = 20
 
     /*
-    1. Conjunction (AND)
-    Operator && mengembalikan nilai true jika semua kondisi yang dievaluasi bernilai true.
+    Int (32 Bit)
+    Int adalah tipe data yang umumnya digunakan untuk menyimpan nilai numerik.
+    Int dapat menyimpan data dari range -2^31 sampai +2^31-1.
     */
-    val isOpen = now >= officeOpen && now <= officeClosed
-
-    println("Office is open : $isOpen")
+    val intNumber = 100
 
     /*
-    2. Disjunction (OR)
-    Operator || mengembalikan nilai true jika salah satu kondisi yang dievaluasi bernilai true.
+    Long (64 Bit)
+    Long adalah tipe data yang digunakan untuk menyimpan nilai numerik yang lebih besar yaitu dari range -2^63 sampai +2^63-1.
     */
-    val isClose = now < officeOpen || now > officeClosed
-
-    println("Office is closed : $isClose")
+    val longNumber: Long = 100 // atau val longNumber = 100L
 
     /*
-    3. Negation (NOT)
-    Operator ! digunakan untuk melakukan negasi pada hasil evaluasi kondisi, mengubah true menjadi false dan sebaliknya.
+    Short (16 Bit)
+    Short merupakan sebuah bilangan bulat yang hanya dapat menyimpan nilai yang kecil karena hanya berukuran 16 Bit.
     */
-    if (!isOpen) {
-        println("Office is closed")
-    } else {
-        println("Office is open")
-    }
+    val shortNumber: Short = 10
+
+    /*
+    Byte (8 Bit)
+    Dengan ukuran yang kecil, Byte hanya mampu menyimpan nilai yang kecil sama halnya seperti Short.
+    Byte biasa digunakan untuk keperluan proses membaca dan menulis data dari sebuah stream file atau jaringan.
+    */
+    val byteNumber = 0b11010010
+
+    /*
+    Double (64 Bit)
+    Sama halnya dengan Long yang memiliki ukuran yang besar, Double mampu menyimpan nilai numerik yang besar pula.
+    Pada umumnya Double digunakan untuk menyimpan nilai numerik pecahan sampai dengan maksimal 15-16 angka di belakang koma.
+    */
+    val doubleNumber: Double = 1.3
+
+    /*
+    Float (32 Bit)
+    Sama seperti Double, namun memiliki ukuran yang lebih kecil, yakni hanya sampai 6-7 angka di belakang koma.
+    */
+    val floatNumber: Float = 0.123456789f    // yang terbaca hanya 0.1234567
+
+//  Cara mengetahui nilai maksimal dan minimal
+    val maxInt = Int.MAX_VALUE
+    val minInt = Int.MIN_VALUE
+    println(maxInt)
+    println(minInt)
+
+// Jika memasukkan nilai lebih dari maksimal maka akan terjadi overflow
+    val overRangeInt = Int.MAX_VALUE + 1 // This operation has led to an overflow
+
+    println("Max Int: $maxInt")
+    println("Over range Int: $overRangeInt")
+
+
+    /*
+    Beberapa fungsi konversi yang dapat digunakan dalam tipe data numbers
+    toByte(): Byte
+    toShort(): Short
+    toInt(): Int
+    toLong(): Long
+    toFloat(): Float
+    toDouble(): Double
+    toChar(): Char
+
+    Contoh :
+    */
+    val stringNumber = "23"
+    val integerNumber = 3
+
+    print(integerNumber + stringNumber.toInt())
 }
