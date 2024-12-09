79cc5cc49c75c6fbb2e9c7673af0d6a42f0d82e0
aryasetiap <aryasetiap.code@gmail.com>
2024-12-09 16:56:36 +0700

    belajar data type dan variabel

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,11 @@ package com.dicoding.kotlin
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+//    Gunakan val jika untuk variabel yang tidak bisa diubah
+//    Gunakan var jika untuk variabel yang bisa diubah
+    val firstName = "Arya"
+    val midleName = "Setia"
+    val lastName = "Pratama"
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    print("$firstName $midleName $lastName")
 }
