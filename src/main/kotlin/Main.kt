869d14f3065a4baef4afc3cdaf085ca99e2a555b
aryasetiap <aryasetiap.code@gmail.com>
2024-12-09 19:25:59 +0700

    belajar If expressions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,22 @@ package com.dicoding.kotlin
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Arya"
-    val old = 20
-    print("My name is $name, im $old years old")
+    val openHours = 7
+    val now = 7
+    val office: String = if (now > 7) {
+        "Office already open"
+    } else if (now == openHours){
+        "Wait a minute, office will be open"
+    } else {
+        "Office is closed"
+    }
 
-//    String Template
-//    Sebuah fitur yang memungkinkan kita untuk menyisipkan
-//    sebuah variabel ke dalam sebuah String tanpa concatenation
-//    (penggabungan objek String menggunakan +)
+    print(office)
+
+//    If expressions digunakan untuk menginisialisasi nilai variabel berdasarkan kondisi tertentu.
+//    If akan mengeksekusi statement atau expression jika kondisi bernilai true,
+//    dan melewatkan proses jika bernilai false.
+//    Jika if digunakan untuk mengembalikan atau menetapkan nilai variabel,
+//    maka harus memiliki branch else.
+//    Kotlin tidak mendukung ternary operator karena perannya sudah digantikan oleh if expressions
 }
