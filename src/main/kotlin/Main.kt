aa49f326fdbc862b3744da9ff834700d95baaaed
aryasetiap <aryasetiap.code@gmail.com>
2024-12-09 17:09:24 +0700

    belajar tipe data string

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,10 @@ package com.dicoding.kotlin
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var vocal = 'A'
+    val text  = "Kotlin"
+    for (char in text){
+        print("$char ")
+    }
 
-    println("Vocal " + vocal++)
-    println("Vocal " + vocal++)
-    println("Vocal " + vocal++)
-    println("Vocal " + vocal--)
-    println("Vocal " + vocal--)
-    println("Vocal " + vocal--)
-    println("Vocal " + vocal--)
-
-    /*
-    Increment dan Decrement dapat digunakan pada tipe data char.
-    Karena pada dasarnya setiap Characters merupakan representasi dari Unicode.
-    Contoh Unicode A adalah 0041.
-    Ketika kita melakukan increment maka hasilnya adalah 0042 yang mana merupakan Unicode dari B.
-    */
+//    String seperti array dan dapat dilakukan indexing
 }
