67729a2f9b853bf59c3b2031a9fe90bd424791ff
aryasetiap <aryasetiap.code@gmail.com>
2024-12-09 17:12:07 +0700

    belajar Escaped String

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,16 @@ package com.dicoding.kotlin
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val text  = "Kotlin"
-    for (char in text){
-        print("$char ")
-    }
+    val statement = "Kotlin is \"Awesome!\""
+    val name = "Unicode test: \u00A9"
 
-//    String seperti array dan dapat dilakukan indexing
+//    Escaped String memungkinkan kita untuk mengurangi ambiguitas nilai yang berada di dalam sebuah String.
+//    \t: menambah tab ke dalam teks.
+//    \n: membuat baris baru di dalam teks.
+//    \’: menambah karakter single quote kedalam teks.
+//    \”: menambah karakter double quote kedalam teks.
+//    \\: menambah karakter backslash kedalam teks.
+
+    println(statement)
+    print(name)
 }
