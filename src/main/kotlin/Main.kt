882004a6187e7a7ba878a4a5fd5b011b500048e3
aryasetiap <aryasetiap.code@gmail.com>
2024-12-09 20:47:44 +0700

    belajar Functions dalam pemrograman Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,21 +2,22 @@ package com.dicoding.kotlin
 
 fun main() {
     /*
-    Safe Calls dan Elvis Operator dalam pemrograman Kotlin.
-    Safe Calls (?.) digunakan untuk mengakses atau mengelola nilai dari objek nullable dengan aman,
-    sehingga mencegah terjadinya NullPointerException. Jika objek bernilai null, proses akan dilewatkan.
+    Functions dalam pemrograman Kotlin. Fungsi adalah prosedur yang memiliki keterkaitan dengan pesan dan objek.
+    Ketika kita memanggil sebuah fungsi, sebuah mini-program akan dijalankan.
+    Fungsi digunakan untuk mengatur program dan dapat mengembalikan nilai.
+    Deklarasi fungsi di Kotlin diawali dengan kata kunci fun, diikuti dengan nama fungsi, parameter, dan tipe kembalian.
+    Fungsi dapat memiliki satu atau lebih parameter yang dipisahkan oleh koma dan berada di dalam tanda kurung.
+    Tipe kembalian adalah nilai yang akan dikembalikan ketika fungsi dipanggil. Jika fungsi hanya memiliki satu expression,
+    maka dapat menggunakan expression body dengan tanda =.
+    Jika fungsi tidak mengembalikan nilai, dapat menggunakan Unit sebagai tipe kembalian.
     */
-    val text: String? = null
-    text?.length // ini akan dilewati karena text bernilai Null Pointer Exception (NPE)
 
-    /*
-    Elvis Operator (?:) memungkinkan kita untuk menetapkan nilai default jika objek bernilai null.
-    Operator ini membantu menulis kode yang lebih aman dan mudah dibaca.
-    */
-    // akan mengembalikan nilai text.length jika text tidak bernilai null
-    // jika null maka akan mengembalikan nilai defaultnya yaitu 7
-    val textLength = text?.length ?: 7
+    fun setUser(name: String, age: Int): String {
+        return "Your name is $name, and you $age years old"
+    }
 
-    println(textLength)
+    fun setUserEB(name: String, age: Int): String = "Your name is $name, and you $age years old"
 
+    println(setUser("Arya", 20))
+    println(setUserEB("Bela", 21))
 }
