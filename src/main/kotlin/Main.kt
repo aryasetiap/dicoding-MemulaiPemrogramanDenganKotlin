de398b96fe6bf4008998a215aa22f9bf954cba2a
aryasetiap <aryasetiap.code@gmail.com>
2024-12-09 19:18:50 +0700

    belajar String Template

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,12 @@ package com.dicoding.kotlin
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val line = """
-        Line 1
-        Line 2
-        Line 3
-        Line 4
-    """.trimIndent()
+    val name = "Arya"
+    val old = 20
+    print("My name is $name, im $old years old")
 
-    print(line)
-
-//    Raw String memungkinkan untuk menuliskan multiline dan arbitrary text.
+//    String Template
+//    Sebuah fitur yang memungkinkan kita untuk menyisipkan
+//    sebuah variabel ke dalam sebuah String tanpa concatenation
+//    (penggabungan objek String menggunakan +)
 }
