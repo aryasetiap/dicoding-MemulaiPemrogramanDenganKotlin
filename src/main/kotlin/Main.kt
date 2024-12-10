886a99f9a52bfc96dbb039d71da8768fb0f0471a
aryasetiap <aryasetiap.code@gmail.com>
2024-12-10 19:25:09 +0700

    belajar While dan Do While dalam pemrograman Kotlin.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,34 @@
 package com.dicoding.kotlin
 
-import kotlin.random.Random
-
 fun main() {
     /*
-    When Expressions dalam pemrograman Kotlin. When expressions digunakan sebagai alternatif dari if-else if yang lebih
-    sederhana dan lebih mudah dibaca. When expression memungkinkan kita untuk mencocokkan nilai dari sebuah variabel
-    atau expression dengan beberapa kondisi secara berurutan sampai salah satu kondisi terpenuhi. Kita juga bisa
-    menambahkan branch else yang akan dievaluasi jika tidak ada kondisi yang terpenuhi. When expression dapat
-    mengembalikan nilai dan dapat disimpan dalam sebuah variabel. Selain itu, when expression juga bisa digunakan untuk
-    memeriksa instance dengan tipe tertentu dari sebuah objek menggunakan is atau !is, serta memeriksa nilai yang
-    terdapat pada sebuah Range atau Collection.
+    While dan Do While dalam pemrograman Kotlin.
+
+    While Loop: Digunakan untuk menjalankan blok kode selama kondisi tertentu terpenuhi. While loop bersifat entry
+    controlled loop, artinya kondisi dievaluasi sebelum blok kode dijalankan. Jika kondisi tidak terpenuhi sejak awal,
+    blok kode tidak akan dijalankan sama sekali.
+
+    Do While Loop: Berbeda dengan while loop, do while loop bersifat exit controlled loop. Ini berarti blok kode akan
+    dijalankan setidaknya sekali sebelum kondisi dievaluasi. Jika kondisi terpenuhi, blok kode akan dijalankan kembali.
+
+    Kedua jenis loop ini digunakan untuk menghindari penulisan kode yang berulang dan memungkinkan perulangan blok kode
+    berdasarkan kondisi tertentu. Halaman ini juga memberikan contoh penggunaan while dan do while loop serta
+    memperingatkan tentang infinite loop, yaitu kondisi di mana perulangan berlangsung terus menerus sampai aplikasi crash
     */
 
-    val registerNumber = when(val regis = getRegisterNumber()){
-        in 1..50 -> 50 * regis
-        in 51..100 -> 100 * regis
-        else -> regis
+    // While
+    var counter = 1
+    while (counter <= 7){
+        println("Hello, World!")
+        counter++
     }
 
-    println(registerNumber)
-}
+    println()
 
-fun getRegisterNumber() = Random.nextInt(100)
+    // Do While
+    var counter2 = 1
+    do {
+        println("Hello, World!")
+        counter2++
+    } while (counter2 <= 7)
+}
