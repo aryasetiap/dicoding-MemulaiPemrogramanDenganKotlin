42cc0747fc5a275d4ddf3ca7646d29755d954ef8
aryasetiap <aryasetiap.code@gmail.com>
2024-12-10 19:54:45 +0700

    belajar Range dalam pemrograman Kotlin.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,33 +2,18 @@ package com.dicoding.kotlin
 
 fun main() {
     /*
-    While dan Do While dalam pemrograman Kotlin.
+    Range dalam pemrograman Kotlin.
 
-    While Loop: Digunakan untuk menjalankan blok kode selama kondisi tertentu terpenuhi. While loop bersifat entry
-    controlled loop, artinya kondisi dievaluasi sebelum blok kode dijalankan. Jika kondisi tidak terpenuhi sejak awal,
-    blok kode tidak akan dijalankan sama sekali.
-
-    Do While Loop: Berbeda dengan while loop, do while loop bersifat exit controlled loop. Ini berarti blok kode akan
-    dijalankan setidaknya sekali sebelum kondisi dievaluasi. Jika kondisi terpenuhi, blok kode akan dijalankan kembali.
-
-    Kedua jenis loop ini digunakan untuk menghindari penulisan kode yang berulang dan memungkinkan perulangan blok kode
-    berdasarkan kondisi tertentu. Halaman ini juga memberikan contoh penggunaan while dan do while loop serta
-    memperingatkan tentang infinite loop, yaitu kondisi di mana perulangan berlangsung terus menerus sampai aplikasi crash
+    Range adalah tipe data unik yang memungkinkan kita menentukan nilai awal dan nilai akhir. Range direpresentasikan
+    dengan operator .., fungsi rangeTo(), atau downTo(). Kita bisa menentukan jarak antara dua nilai yang dicakup oleh
+    Range menggunakan properti step. Kotlin mendukung beberapa tipe integral seperti IntRange, LongRange, dan CharRange,
+    sehingga kita bisa menggunakan nilai numerik maupun karakter dalam Range. Selain itu, kita juga bisa memeriksa
+    apakah suatu nilai ada atau tidak ada dalam cakupan Range menggunakan kata kunci in dan !in.
     */
 
-    // While
-    var counter = 1
-    while (counter <= 7){
-        println("Hello, World!")
-        counter++
+    val rangeInt = 1..10 step 2
+    rangeInt.forEach {
+        print("$it ")
     }
-
-    println()
-
-    // Do While
-    var counter2 = 1
-    do {
-        println("Hello, World!")
-        counter2++
-    } while (counter2 <= 7)
+    println(rangeInt.step)
 }
