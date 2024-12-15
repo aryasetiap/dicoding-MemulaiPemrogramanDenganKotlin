f61e7c4f55e1d203fc184f356fcf55758bae7c95
aryasetiap <aryasetiap.code@gmail.com>
2024-12-15 16:06:46 +0700

    belajar Primary Constructor dalam Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,80 +1,43 @@
 package com.dicoding.kotlin
 
-import kotlin.reflect.KProperty
+class Animal(val name: String, val weight: Double, val age: Int, val isMammal: Boolean)
+// dengan nilai default
+// class Animal(var name: String, var weight: Double, var age: Int = 0, var isMammal: Boolean = true)
 
-// delegate properti string
-class DelegateName {
-    private var value: String = "Default"
+// init block : untuk melakukan verivikasi terhadap nilai properti
+class Animal2(name: String, weight: Double, age: Int, isMammal: Boolean){
+    val name: String
+    val weight: Double
+    val age: Int
+    val isMammal: Boolean
 
-    operator fun getValue(classRef: Any?, property: KProperty<*>) : String {
-        println("Fungsi ini sama seperti getter untuk properti ${property.name} pada class $classRef")
-        return value
-    }
-
-    operator fun setValue(classRef: Any?, property: KProperty<*>, newValue: String){
-        println("Fungsi ini sama seperti setter untuk properti ${property.name} pada class $classRef")
-        println("Nilai ${property.name} dari: $value akan berubah menjadi $newValue")
-        value = newValue
-    }
-}
-
-class Person {
-    var name: String by DelegateName()
-}
-
-class Hero {
-    var name: String by DelegateName()
-}
-
-// delegate properti any
-class DelegateGenericClass {
-    private var value: Any = "Default"
-
-    operator fun getValue(classRef: Any, property: KProperty<*>): Any {
-        println("Fungsi ini sama seperti getter untuk properti ${property.name} pada class $classRef")
-        return value
-    }
-
-    operator fun setValue(classRef: Any, property: KProperty<*>, newValue: Any) {
-        println("Nilai ${property.name} dari: $value akan berubah menjadi $newValue")
-        value = newValue
+    init {
+        this.weight = if(weight < 0) 0.1 else weight
+        this.age = if(age < 0) 0  else age
+        this.name = name
+        this.isMammal = isMammal
     }
 }
 
-class Animal {
-    var name: Any by DelegateGenericClass()
-    var weight: Any by DelegateGenericClass()
-    var age: Any by DelegateGenericClass()
-}
-
-fun main() {
+fun main(){
     /*
-    Property Delegation dalam Kotlin:
+    Primary Constructor dalam Kotlin:
 
-    1. Pengelolaan Properti: Properti kelas dapat didelegasikan kepada kelas lain untuk meminimalisir penulisan kode getter
-    dan setter yang berulang.
+    Primary Constructor: Digunakan untuk menginisialisasi properti pada kelas saat objek dibuat. Properti dituliskan
+    pada header class dengan var atau val.
 
-    2. Kelas Delegasi: Sebelum mendelegasikan properti, buat kelas delegasi terlebih dahulu. Gunakan keyword by untuk
-    menginisialisasi properti yang didelegasikan.
+    Nilai Default: Primary constructor dapat memiliki nilai default untuk properti, sehingga pengiriman nilai pada saat
+    pembuatan objek bersifat opsional.
 
-    2. Contoh Kode: Properti name dikelola melalui kelas DelegateName. Delegasi dapat digunakan oleh properti dengan tipe
-    data yang berbeda menggunakan tipe data Any.
+    Init Block: Kotlin menyediakan blok init untuk menginisialisasi properti di dalam body class dan memvalidasi nilai
+    properti sebelum diinisialisasi.
+
+    Keyword this: Digunakan untuk menghindari ambiguitas antara properti pada body class dan parameter pada head class.
     */
 
-    val animal = Animal()
-    animal.name = "Dicoding cat"
-    animal.weight = 6.2
-    animal.age = 1
+    val dicodingCat = Animal("Dicoding Miaw", 4.2, 2, true)
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, mamalia: ${dicodingCat.isMammal}" )
 
-    println("Nama: ${animal.name}")
-    println("Berat: ${animal.weight}")
-    println("Umur: ${animal.age} Tahun")
-
-    val person = Person()
-    person.name = "Dimas"
-    println("Nama Orang: ${person.name}")
-
-    val hero = Hero()
-    hero.name = "Gatotkaca"
-    println("Nama Pahlawan: ${hero.name}")
+    val dicodingCat2 = Animal2("Dicoding Miaw", 4.2, 2, true)
+    println("Nama: ${dicodingCat2.name}, Berat: ${dicodingCat2.weight}, Umur: ${dicodingCat2.age}, mamalia: ${dicodingCat2.isMammal}" )
 }
